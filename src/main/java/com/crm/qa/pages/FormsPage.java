4e03ddcc09e4a57345077d726ea006ef03d30f21
Kp-Kalpesh <kalpeshpatil089136@gmail.com>
2025-09-04 12:53:31 +0530

    Forms webElements

--- src/main/java/com/crm/qa/pages/FormsPage.java
+++ src/main/java/com/crm/qa/pages/FormsPage.java
@@ -17,11 +17,32 @@ public class FormsPage extends TestBase{
 
 	@FindBy(xpath = "//span[text()='Practice Form']")
 	WebElement practiceForm;
-
-
-
-
-
+	@FindBy(xpath = "//h5[text()='Student Registration Form']")
+	WebElement studentRegiFormText;
+	@FindBy(xpath = "//input[contains(@placeholder,'First Name')]")
+	WebElement firstName;
+	@FindBy(xpath = "//input[contains(@placeholder,'Last Name')]")
+	WebElement lastName;
+	@FindBy(xpath = "//input[contains(@placeholder,'name@example.com')]")
+	WebElement email;
+	@FindBy(xpath = "(//input[contains(@name,'gender')])[1]")
+	WebElement genderRadio;
+	@FindBy(xpath = "//input[contains(@placeholder,'Mobile Number')]")
+	WebElement mobNumber;
+	@FindBy(xpath = "//input[contains(@id,'dateOfBirthInput')]")
+	WebElement dob;
+	@FindBy(xpath = "//div[contains(@class,'subjects-auto-complete__value')]")
+	WebElement subjectField;
+	@FindBy(xpath = "(//input[contains(@type,'checkbox')])[1]")
+	WebElement hobbiesChkBx;
+	@FindBy(xpath = "(//textarea[contains(@placeholder,'Current Address')])[1]")
+	WebElement currentAddress;
+	@FindBy(xpath = "(//div[contains(text(),'Select State')])[1]")
+	WebElement selectStateDrpDwn;
+	@FindBy(xpath = "(//div[contains(@class,' css-1hwfws3')])[3]")
+	WebElement selectCityDrpDwn;
+	@FindBy(xpath = "//button[contains(@id,'submit')]")
+	WebElement submitBtn;
 
 
 
@@ -33,10 +54,9 @@ public class FormsPage extends TestBase{
 	}
 
 	public String verifyFormsText() {
-//		Forms.sendKeys(Keys.PAGE_DOWN);
+		//		Forms.sendKeys(Keys.PAGE_DOWN);
 		return Forms.getText();
 	}
-
 
 	public String verifyPracticeFormText() throws InterruptedException {
 		Thread.sleep(2000);
